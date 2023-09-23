aba17efc88a8dcdb6e3f8c15ca77e08dacb1c91f
William Loring <32806514+itinstructor@users.noreply.github.com>
2023-09-23 06:29:41 -0600

    commit message

---
+++ JokeAPIV2.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+
+
+namespace JokeAPIV2
+{
+    class Program
+    {
+        // JokeAPI URL for Any joke in safe-mode
+        // dark, explicit jokes are not retrieved
+        private const string API = "https://v2.jokeapi.dev/joke/Any?&safe-mode";
+        static void Main(string[] args)
+        {
+            // Create the http WebClient to GET API call
+            WebClient client = new WebClient();
+
+            // Create string to store Json data
+            string jsonString;
+            
+            // GET the json data, store in jsonString
+            jsonString = client.DownloadString(API);
+            
+            // Convert Json into C# Class
+            JokeAPIV2.Joke joke = JsonConvert.DeserializeObject<JokeAPIV2.Joke>(jsonString);
+
+            // For debugging, display raw Json data
+            // Console.WriteLine(jsonString);
+
+            Console.WriteLine("Get a joke\n");
+
+            // Jok is displayed as properties of joke object
+            Console.WriteLine($"{joke.setup}");
+            Console.WriteLine($"{joke.delivery}");
+
+            // Pause the program
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ JokeAPIV2/Joke.cs
@@ -0,0 +1,31 @@
+﻿/**
+* Project:       JokeAPIV2
+* Programmer:    William Loring
+* Date:          03/12/2021
+* Description:   Base class for JokeAPI library wrapper
+ */
+namespace JokeAPIV2
+{
+    public class Joke
+    {
+        public bool error { get; set; }
+
+        public string category { get; set; }
+
+        public string type { get; set; }
+
+        // Retrieve the joke setup
+        public string setup { get; set; }
+
+        // Retrieve the joke delivery
+        public string delivery { get; set; }
+
+        public bool flag { get; set; }
+
+        public bool safe { get; set; }
+
+        public long id { get; set; }
+
+        public string lang { get; set; }
+    }
+}
